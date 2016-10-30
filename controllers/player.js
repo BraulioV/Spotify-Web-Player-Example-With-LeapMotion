e669bb27427ca8b3df1adc48554399595976640d
mgmacias95 <mgmacias95@correo.ugr.es>
2016-10-30 20:45:31 +0100

    ya le da al play all cuando tienes el dedo encima!

--- controllers/player.js
+++ controllers/player.js
@@ -1,9 +1,33 @@
 (function() {
 
 	var module = angular.module('PlayerApp');
-	var eventDispatched = false;		// temporizador para leapç
-	var pointer = document.createElement( 'div' );
+	var eventDispatched = false;		// temporizador para leap
+	var pointer = document.createElement( 'div' );	// puntero azul
 	pointer.id = 'leap';
+	// obtenemos todos los enlaces del documento, menos el botón verde "play all"
+	var enlaces = document.documentElement.getElementsByClassName("ng-binding");
+	// obtenemos el botón de play all de una playlist
+	var playallbutton = document.documentElement.getElementsByClassName("button green");
+	// variables para saber dónde apuntaba antes el pointer
+	var old_pointer_t = 0;
+	var old_pointer_l = 0;
+	var cur_pointer_t = 0;
+	var cur_pointer_l = 0;
+	var temporizador = false;
+	// función para saber la localización de un elemento
+	function get_offset_parent(kind, element) {
+		var local_element = element;
+		var offset = 0;
+		while (local_element.offsetParent != null) {
+			if (kind == "left") {
+				offset += local_element.offsetParent.offsetLeft;
+			} else {
+				offset += local_element.offsetParent.offsetTop;
+			}
+			local_element = local_element.offsetParent;
+		}
+		return offset;
+	}
 
 	module.controller('PlayerController', function($scope, $rootScope, Auth, API, PlayQueue, Playback, $location) {
 		$scope.view = 'welcome';
@@ -189,31 +213,26 @@
 							}
 						case "screenTap":
 							console.log("screen tap");
-							// obtenemos todos los enlaces del documento, menos el botón verde "play all"
-							// var enlaces = document.documentElement.getElementsByClassName("ng-binding");
-							// console.log(enlaces);
-							// obtenemos el botón de play all de una playlist
-							var playallbutton = document.documentElement.getElementsByClassName("button green");
 							// una vez tenemos todos los enlaces, vemos en cuál ha hecho click el usuario.
 							// Para ello, nos basaremos en la posición del screen tap y en los atributos
 							// offsetLeft y offsetTop de cada enlace
 							
-							// obtenemos el dedo con el que hemos hecho screen tap
-							function has_same_id(dedo) {
-								return dedo.id == gesture.pointableIds[0];
-							}
-							var dedo = frame.fingers.filter(has_same_id)[0];
+							// // obtenemos el dedo con el que hemos hecho screen tap
+							// function has_same_id(dedo) {
+							// 	return dedo.id == gesture.pointableIds[0];
+							// }
+							// var dedo = frame.fingers.filter(has_same_id)[0];
 
-							// calculamos la posición del screen tap sobre la pantalla
-							var pos_top = (1 - ((dedo.tipPosition[1] - 50) / 120)) * body.offsetHeight;
-							var pos_left = (dedo.tipPosition[0] * body.offsetWidth / 120) + (body.offsetWidth / 2);
+							// // calculamos la posición del screen tap sobre la pantalla
+							// var pos_top = (1 - ((dedo.tipPosition[1] - 50) / 120)) * body.offsetHeight;
+							// var pos_left = (dedo.tipPosition[0] * body.offsetWidth / 120) + (body.offsetWidth / 2);
 
-							console.log("dedo")
-							console.log(pos_top);
-							console.log(pos_left);
-							console.log("botón")
-							console.log(playallbutton[0].offsetParent.offsetTop);
-							console.log(playallbutton[0].offsetParent.offsetLeft);
+							// console.log("dedo")
+							// console.log(pos_top);
+							// console.log(pos_left);
+							// console.log("botón")
+							// console.log(offset_t);
+							// console.log(offset_l);
 
 						break;
 					}
@@ -240,12 +259,49 @@
 					pointer.style.borderRadius = size - 5 + 'px';
 
 					if (finger.extended) {
-						pointer.style.visibility   = 'visible';
-						pointer.style.top  = ( 1 - (( finger.tipPosition[1] - 50) / 120 )) *
-		          		body.offsetHeight + 'px';
+						if (!temporizador) {
+							temporizador = true;
+							pointer.style.visibility   = 'visible';
+							cur_pointer_t  = ( 1 - (( finger.tipPosition[1] - 50) / 120 )) *
+			          		body.offsetHeight;
+			          		pointer.style.top = cur_pointer_t + 'px';
 
-		        		pointer.style.left = ( finger.tipPosition[0] * body.offsetWidth / 120 ) +
-						( body.offsetWidth / 2 ) + 'px';
+			        		cur_pointer_l = ( finger.tipPosition[0] * body.offsetWidth / 120 ) +
+							( body.offsetWidth / 2 );
+							pointer.style.left = cur_pointer_l + 'px';
+
+							old_pointer_l = cur_pointer_l;
+							old_pointer_t = cur_pointer_t;
+						} else {
+							pointer.style.visibility   = 'visible';
+							cur_pointer_t  = ( 1 - (( finger.tipPosition[1] - 50) / 120 )) *
+			          		body.offsetHeight;
+			          		pointer.style.top = cur_pointer_t + 'px';
+
+			        		cur_pointer_l = ( finger.tipPosition[0] * body.offsetWidth / 120 ) +
+							( body.offsetWidth / 2 );
+							pointer.style.left = cur_pointer_l + 'px';
+
+							if (Math.abs(cur_pointer_t - old_pointer_t) <= 20 &&
+								Math.abs(cur_pointer_l - old_pointer_l) <= 20) {
+								console.log("yay");
+								var offset_l = get_offset_parent("left", playallbutton[0]);
+								var offset_t = get_offset_parent("top", playallbutton[0]);
+								console.log("offset_l = ", offset_l);
+								console.log("offset_t = ", offset_t);
+								console.log("cur_pointer_l = ", cur_pointer_l);
+								console.log("cur_pointer_t = ", cur_pointer_t)
+								if (Math.abs(cur_pointer_t - offset_t) <= 50) {
+									console.log("click");
+									playallbutton[0].click();
+								}
+							} else {
+								console.log("nay");
+							}
+						}
+						window.setTimeout(function() {
+							temporizador = false;
+						}, 4000);
 					}
 				});
 			} else {
